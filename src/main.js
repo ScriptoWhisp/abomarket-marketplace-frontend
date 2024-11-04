3e297b9acc7391ba2b4b5e9039cf92848cd628fd
datjul <datjul@taltech.ee>
2024-11-04 13:38:47 +0200

    login

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 
+const token = localStorage.getItem('user_token')
+
+if (token) {
+  axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
+}
+
 const app = createApp(App)
 
 app.provide('axios', axios)
