97693ea435e49ac3f2e57ff2d9aee6464fb6bb06
vidovb <vidovb@taltech.ee>
2024-11-13 09:11:49 +0200

    quick fix for location field (missing v-model)

--- src/views/UserPage.vue
+++ src/views/UserPage.vue
@@ -155,7 +155,7 @@ onMounted(getUsersData)
                 <div class="col-span-2">
                   <label for="location" class="block text-sm/6 font-medium text-gray-900">Location</label>
                   <div class="mt-2">
-                    <input id="location" name="location" type="text" class="block w-full rounded-md border-0 p-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6" />
+                    <input v-model="location" id="location" name="location" type="text" class="block w-full rounded-md border-0 p-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6" />
                   </div>
                 </div>
               </div>
