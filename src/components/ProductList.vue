334dc209c3057cade1df16bbc31075439900c7b5
alexsm <alexsm@taltech.ee>
2024-11-21 20:48:20 +0200

    Added sorting and filtering

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -8,12 +8,22 @@ const products = ref([]);
 const currentPage = ref(1);
 const pageSize = 10;
 const totalPages = ref(1);
+const categories = ref([])
 
 const error = ref('');
+const activeCategory = ref(null);
+const activeSort = ref("productId");
+const activeSortDirection = ref("DESC");
 
 const fetchProducts = async () => {
   try {
-    const response = await axios.get(`/api/products?pageNo=${currentPage.value - 1}&pageSize=${pageSize}`);
+    let url = `/api/products?sortBy=${activeSort.value}&sortDirection=${activeSortDirection.value}&pageNo=${currentPage.value - 1}&pageSize=${pageSize}`;
+
+    if (activeCategory.value !== null) {
+      url += `&category=${activeCategory.value}`;
+    }
+
+    const response = await axios.get(url);
     console.log(response);
     products.value = response.data.content.map(product => ({
       id: product.id,
@@ -27,6 +37,51 @@ const fetchProducts = async () => {
   }
 };
 
+const fetchCategories = async () => {
+  try {
+    const response = await axios.get(`/api/categories`);
+    console.log(response);
+    categories.value = response.data.map(category => ({
+      id: category.id,
+      name: category.name
+    }));
+  } catch (err) {
+    error.value = 'Error fetching categories: ' + err.message;
+  }
+};
+
+// for sorting by category
+const filterByCategory = (categoryId) => {
+  if (activeCategory.value === categoryId) {
+    activeCategory.value = null;
+  } else {
+    activeCategory.value = categoryId;
+  }
+  fetchProducts()
+};
+
+const selectSortOption = (option) => {
+  // Обновляем сортировку в зависимости от выбора
+  switch (option) {
+    case 'new-first':
+      activeSort.value = 'productId';
+      activeSortDirection.value = 'DESC';
+      break;
+    case 'old-first':
+      activeSort.value = 'productId';
+      activeSortDirection.value = 'ASC';
+      break;
+    case 'lowest-price':
+      activeSort.value = 'price';
+      activeSortDirection.value = 'ASC';
+      break;
+    case 'highest-price':
+      activeSort.value = 'price';
+      activeSortDirection.value = 'DESC';
+      break;
+  }
+  fetchProducts();
+};
 
 const nextPage = () => {
   currentPage.value++;
@@ -38,22 +93,73 @@ const previousPage = () => {
   fetchProducts();
 };
 
-onMounted(fetchProducts);
+onMounted(() => {
+  fetchProducts();
+  fetchCategories();
+});
 
 </script>
 
 <template>
   <div v-if="error" class="error-message">{{ error }}</div>
 
-  <div class="bg-white">
-    <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+  <div class="bg-white flex">
 
+    <aside class="w-1/4 min-h-screen bg-white border-r border-gray-300 px-4 py-8">
+      <!-- Sort Section -->
+      <h3 class="text-lg font-semibold mb-6">Sort</h3>
+      <div class="flex flex-col space-y-4">
+        <!-- Sort by New/Old and Price -->
+        <button
+            @click="selectSortOption('new-first')"
+            :class="{'bg-gray-400': activeSort === 'productId' && activeSortDirection === 'DESC', 'bg-gray-200': activeSort !== 'productId' || activeSortDirection !== 'DESC'}"
+            class="flex items-center justify-center py-2 px-4 text-black rounded hover:scale-105"
+        >
+          New products first
+        </button>
+        <button
+            @click="selectSortOption('old-first')"
+            :class="{'bg-gray-400': activeSort === 'productId' && activeSortDirection === 'ASC', 'bg-gray-200': activeSort !== 'productIDd' || activeSortDirection !== 'ASC'}"
+            class="flex items-center justify-center py-2 px-4 text-black rounded hover:scale-105"
+        >
+          Old products first
+        </button>
+        <button
+            @click="selectSortOption('lowest-price')"
+            :class="{'bg-gray-400': activeSort === 'price' && activeSortDirection === 'ASC', 'bg-gray-200': activeSort !== 'price' || activeSortDirection !== 'ASC'}"
+            class="flex items-center justify-center py-2 px-4 text-black rounded hover:scale-105"
+        >
+          Lowest price first
+        </button>
+        <button
+            @click="selectSortOption('highest-price')"
+            :class="{'bg-gray-400': activeSort === 'price'  && activeSortDirection === 'DESC', 'bg-gray-200': activeSort !== 'price' || activeSortDirection !== 'DESC'}"
+            class="flex items-center justify-center py-2 px-4 text-black rounded hover:scale-105"
+        >
+          Highest price first
+        </button>
+      </div>
 
+      <!-- Categories Section -->
+      <h3 class="text-lg font-semibold mt-6 mb-6">Categories</h3>
+      <div class="flex flex-col space-y-4">
+        <div v-for="category in categories" :key="category.id">
+          <button
+              @click="filterByCategory(category.id)"
+              :class="{
+              'bg-gray-400': activeCategory === category.id,
+              'bg-gray-200': activeCategory !== category.id
+            }"
+              class="flex items-center justify-center py-2 px-4 text-black rounded hover:scale-105 w-full text-left"
+          >
+            {{ category.name }}
+          </button>
+        </div>
+      </div>
+    </aside>
+
+    <div class="flex-1 mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
       <h2 class="text-black text-5xl mb-10">Products</h2>
-
-      <!-- filter stuff goes here -->
-
-      <!-- and here it ends -->
 
       <div v-if="products.length === 0 || error" class="text-2xl text-gray-500 text-center">Nothing to show</div>
 
